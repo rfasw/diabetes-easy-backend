085a642908dd632a36e8196d4f76a431fa4b0289
sebastian macheso <130545817+rfasw@users.noreply.github.com>
2025-06-20 16:12:26 -0700

    intell

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-# Install specific Python version
-apt-get update && apt-get install -y python3.9 python3.9-dev
-# Make it the default
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-update-alternatives --set python3 /usr/bin/python3.9
-# Install dependencies
-pip install -r requirements.txt
+apt-get update && apt-get install -y python3.9 python3.9-dev && \
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1 && \
+update-alternatives --set python3 /usr/bin/python3.9 && \
+pip install poetry && \
+poetry install --no-dev
